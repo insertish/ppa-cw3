8b18b4b201217abc989557cb18ebbdc6ce2828a7
Paul Makles <paulmakles@gmail.com>
2022-02-08 17:32:45 +0000

    feat(coordinate): implement equals

--- src/main/java/gay/oss/cw3/simulation/Coordinate.java
+++ src/main/java/gay/oss/cw3/simulation/Coordinate.java
@@ -18,7 +18,19 @@ public class Coordinate {
         return new Coordinate(this.x + value.x, this.z + value.z);
     }
 
+    public Coordinate add(int x, int z) {
+        return new Coordinate(this.x + x, this.z + z);
+    }
+
     public String toString() {
         return String.format("Coordinate { x = %d, z = %d }", this.x, this.z);
+    }
+
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (!(obj instanceof Coordinate)) return false;
+
+        Coordinate c = (Coordinate) obj;
+        return c.x == this.x && c.z == this.z;
     }
 }
