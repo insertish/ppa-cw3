91dbb8ed8aca4da9201cc05aa2020dc6583c5653
williambl <william@williambl.com>
2022-02-08 12:50:48 +0000

    chore: improve IKeyCallback javadoc

--- src/main/java/gay/oss/cw3/renderer/interfaces/IKeyCallback.java
+++ src/main/java/gay/oss/cw3/renderer/interfaces/IKeyCallback.java
@@ -1,12 +1,14 @@
 package gay.oss.cw3.renderer.interfaces;
 
+import org.lwjgl.glfw.GLFW;
+
 public interface IKeyCallback {
     /**
      * Will be called when a key is pressed, repeated or released.
      *
      * @param key       the keyboard key that was pressed or released
-     * @param action    the key action. One of:<br><table><tr><td>{@link GLFW#GLFW_PRESS PRESS}</td><td>{@link GLFW#GLFW_RELEASE RELEASE}</td><td>{@link GLFW#GLFW_REPEAT REPEAT}</td></tr></table>
-     * @param modifiers bitfield describing which modifiers keys were held down
+     * @param action    the key action. One of:<ul><li>{@link GLFW#GLFW_PRESS PRESS}</li><li>{@link GLFW#GLFW_RELEASE RELEASE}</li><li>{@link GLFW#GLFW_REPEAT REPEAT}</li></ul>
+     * @param mods bitfield describing which modifiers keys were held down
      */
     void invoke(int key, int action, int mods);
 }
