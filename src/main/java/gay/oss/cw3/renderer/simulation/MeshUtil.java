8d4cb9cbb7c8132130acff514ffd02abb73e97f5
Paul Makles <paulmakles@gmail.com>
2022-02-14 19:31:13 +0000

    feat: squares with basic shading

--- src/main/java/gay/oss/cw3/renderer/simulation/MeshUtil.java
+++ src/main/java/gay/oss/cw3/renderer/simulation/MeshUtil.java
@@ -13,17 +13,58 @@ public class MeshUtil {
             for (int z=0;z<depth-1;z++) {
                 float x0z0 = heightMap.get(x, z) * 10.0f;
                 float x1z0 = heightMap.get(x + 1, z) * 10.0f;
+                float x0z1 = heightMap.get(x, z + 1) * 10.0f;
+                float x1z1 = heightMap.get(x + 1, z + 1) * 10.0f;
 
                 int offset = 36 * ((z * width) + x);
-                vertices[offset + 0] = x;
-                vertices[offset + 1] = x0z0;
-                vertices[offset + 2] = 0.0f + z;
-                vertices[offset + 3] = x;
-                vertices[offset + 4] = x1z0;
-                vertices[offset + 5] = z + 1;
-                vertices[offset + 6] = x + 0.5f;
-                vertices[offset + 7] = (x0z0 + x1z0) / 2;
-                vertices[offset + 8] = z + 0.5f;
+
+                float cx = x + 0.5f;
+                float cy = (x0z0 + x1z0 + x0z1 + x1z1) / 4;
+                float cz = z + 0.5f;
+
+                // Bottom Triangle
+                vertices[offset + 0 ] = x;
+                vertices[offset + 1 ] = x0z0;
+                vertices[offset + 2 ] = z;
+                vertices[offset + 3 ] = x;
+                vertices[offset + 4 ] = x0z1;
+                vertices[offset + 5 ] = z + 1;
+                vertices[offset + 6 ] = cx;
+                vertices[offset + 7 ] = cy;
+                vertices[offset + 8 ] = cz;
+
+                // Left Triangle
+                vertices[offset + 9 ] = x;
+                vertices[offset + 10] = x0z0;
+                vertices[offset + 11] = z;
+                vertices[offset + 12] = cx;
+                vertices[offset + 13] = cy;
+                vertices[offset + 14] = cz;
+                vertices[offset + 15] = x + 1;
+                vertices[offset + 16] = x1z0;
+                vertices[offset + 17] = z;
+
+                // Right Triangle
+                vertices[offset + 18] = x;
+                vertices[offset + 19] = x0z1;
+                vertices[offset + 20] = z + 1;
+                vertices[offset + 21] = cx;
+                vertices[offset + 22] = cy;
+                vertices[offset + 23] = cz;
+                vertices[offset + 24] = x + 1;
+                vertices[offset + 25] = x1z1;
+                vertices[offset + 26] = z + 1;
+
+                // Top Triangle
+                vertices[offset + 27] = cx;
+                vertices[offset + 28] = cy;
+                vertices[offset + 29] = cz;
+                vertices[offset + 30] = x + 1;
+                vertices[offset + 31] = x1z1;
+                vertices[offset + 32] = z + 1;
+                vertices[offset + 33] = x + 1;
+                vertices[offset + 34] = x1z0;
+                vertices[offset + 35] = z;
             }
         }
 
--- src/main/resources/shaders/vertex.glsl
+++ src/main/resources/shaders/vertex.glsl
@@ -5,8 +5,10 @@ layout (location = 1) in vec2 vertexUV;
 uniform mat4 modelViewProjection;
 
 out vec2 fragUV;
+out float yCoord;
 
 void main() {
     gl_Position = modelViewProjection * vec4(vertexPos, 1.0);
     fragUV = vertexUV;
+    yCoord = vertexPos.y;
 }
