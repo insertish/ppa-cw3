8921be254ce86f0e3c5c671f6d809947949ec8af
williambl <william@williambl.com>
2022-02-14 17:09:17 +0000

    chore(entities): remove todo

--- src/main/java/gay/oss/cw3/simulation/entity/brain/behaviours/BreedBehaviour.java
+++ src/main/java/gay/oss/cw3/simulation/entity/brain/behaviours/BreedBehaviour.java
@@ -8,7 +8,6 @@ import org.jetbrains.annotations.Nullable;
 import java.util.*;
 
 //TODO: sexes
-//TODO: attribute averaging
 public class BreedBehaviour<T extends Entity & Breedable> extends MovementBehaviour {
     private final Random random = new Random();
     private final T entity;
