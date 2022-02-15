b26be58fc7e224c5cab08a3acc0dfff5aa967da0
Paul Makles <paulmakles@gmail.com>
2022-02-15 10:17:59 +0000

    feat(biome): add colours to biome enum

--- src/main/java/gay/oss/cw3/simulation/world/BiomeType.java
+++ src/main/java/gay/oss/cw3/simulation/world/BiomeType.java
@@ -1,9 +1,19 @@
 package gay.oss.cw3.simulation.world;
 
 public enum BiomeType {
-    Plains,
-    Forest,
-    Jungle;
+    Plains(new float[] { 0.32f, 0.42f, 0.19f }),
+    Forest(new float[] { 0.60f, 0.50f, 0.29f }),
+    Jungle(new float[] { 0.04f, 0.23f, 0.01f });
+
+    private float[] colour;
+
+    private BiomeType(float[] colour) {
+        this.colour = colour;
+    }
+
+    public float[] getColour() {
+        return this.colour;
+    }
 
     public static BiomeType[] BIOME_TYPES = {
         Plains, Forest, Jungle
