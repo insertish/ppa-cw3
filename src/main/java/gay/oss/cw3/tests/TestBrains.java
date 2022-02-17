caa4c9c4ed8b7eba41b2708dd7631ec0d4be4aa1
williambl <william@williambl.com>
2022-02-17 16:24:52 +0000

    feat: add eat foliage behaviour

--- src/main/java/gay/oss/cw3/tests/TestBrains.java
+++ src/main/java/gay/oss/cw3/tests/TestBrains.java
@@ -1,6 +1,8 @@
 package gay.oss.cw3.tests;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import gay.oss.cw3.provided.Field;
@@ -9,12 +11,11 @@ import gay.oss.cw3.simulation.Coordinate;
 import gay.oss.cw3.simulation.World;
 import gay.oss.cw3.simulation.entity.AbstractBreedableEntity;
 import gay.oss.cw3.simulation.entity.EntityAttribute;
-import gay.oss.cw3.simulation.entity.brain.behaviours.BreedBehaviour;
-import gay.oss.cw3.simulation.entity.brain.behaviours.FleeBehaviour;
-import gay.oss.cw3.simulation.entity.brain.behaviours.HuntBehaviour;
-import gay.oss.cw3.simulation.entity.brain.behaviours.WanderAroundBehaviour;
+import gay.oss.cw3.simulation.entity.brain.behaviours.*;
 import gay.oss.cw3.simulation.entity.Entity;
+import gay.oss.cw3.simulation.world.DayCycle;
 import gay.oss.cw3.simulation.world.EntityLayer;
+import org.jetbrains.annotations.Nullable;
 
 public class TestBrains {
     public static void main(String[] args) {
@@ -23,9 +24,13 @@ public class TestBrains {
         for (int x=0;x<128;x++) {
             for (int z=0;z<128;z++) {
                 if (new Random().nextFloat() < 0.05) {
-                    new EntityCell(world, new Coordinate(x, z));
+                    new Rabbit(world, new Coordinate(x, z));
                 } else if (new Random().nextFloat() < 0.005) {
                     new Hunter(world, new Coordinate(x, z));
+                }
+
+                if (new Random().nextFloat() < 0.3) {
+                    new Grass(world, new Coordinate(x, z));
                 }
             }
         }
@@ -44,7 +49,8 @@ public class TestBrains {
         var view = new SimulatorView(128, 128);
         var f = new Field(128, 128);
         view.setColor(Hunter.class, Color.ORANGE);
-        view.setColor(EntityCell.class, Color.LIGHT_GRAY);
+        view.setColor(Rabbit.class, Color.LIGHT_GRAY);
+        view.setColor(Grass.class, new Color(0x00dd00));
         while (true) {
             lastIter = world.getTime();
             world.tick();
@@ -66,10 +72,11 @@ public class TestBrains {
     }
 
 
-    static class EntityCell extends AbstractBreedableEntity {
-        public EntityCell(World world, Coordinate location) {
+    static class Rabbit extends AbstractBreedableEntity {
+        public Rabbit(World world, Coordinate location) {
             super(world, location, 0, true, EntityLayer.ANIMALS);
             this.getBrain().addBehaviour(new FleeBehaviour(this, 1.0, 10, Hunter.class));
+            this.getBrain().addBehaviour(new EatFoliageBehaviour(this, 1.0, 0.7, Grass.class));
             this.getBrain().addBehaviour(new BreedBehaviour<>(this, 1.0));
             this.getBrain().addBehaviour(new WanderAroundBehaviour(this, 1.0));
 
@@ -92,7 +99,7 @@ public class TestBrains {
 
         @Override
         public Entity createChild(Entity otherParent, Coordinate location) {
-            var result = new EntityCell(this.getWorld(), location);
+            var result = new Rabbit(this.getWorld(), location);
             result.getAttributes().inheritFromParents(this.getAttributes(), otherParent.getAttributes(), 1.0);
             return result;
         }
@@ -105,8 +112,8 @@ public class TestBrains {
 
     static class Hunter extends AbstractBreedableEntity {
         public Hunter(World world, Coordinate location) {
-            super(world, EntityLayer.ANIMALS, location, 0, true);
-            this.getBrain().addBehaviour(new HuntBehaviour(this, 1.3, EntityCell.class));
+            super(world, location, 0, true, EntityLayer.ANIMALS);
+            this.getBrain().addBehaviour(new HuntBehaviour(this, 1.3, 0.7, Rabbit.class));
             this.getBrain().addBehaviour(new BreedBehaviour<>(this, 0.6));
             this.getBrain().addBehaviour(new WanderAroundBehaviour(this, 0.6));
 
@@ -138,6 +145,49 @@ public class TestBrains {
         @Override
         public boolean isCompatible(Entity entity) {
             return true;
+        }
+    }
+
+    static class Grass extends Entity {
+        private final Random random = new Random();
+
+        public Grass(World world, Coordinate location) {
+            super(world, EntityLayer.FOLIAGE, location, 0, true);
+            this.getAttributes().set(EntityAttribute.MAX_HEALTH, 1);
+            this.getAttributes().set(EntityAttribute.MAX_FULLNESS, 3.0);
+            this.setFullness(1.0);
+        }
+
+        @Override
+        public void tick() {
+            if (this.isAlive()) {
+                // photosynthesis
+                if (this.getWorld().getDayCycle() != DayCycle.NIGHT) {
+                    this.addFullness(0.2);
+                }
+
+                // spreading
+                if (this.getFullness() >= 2.0) {
+                    List<Coordinate> locations = new ArrayList<>();
+
+                    for (int dX = -1; dX <= 1; dX++) {
+                        for (int dZ = -1; dZ <= 1; dZ++) {
+                            var coord = this.getLocation().add(dX, dZ);
+
+                            if (this.getWorld().isInBounds(coord) && this.getWorld().getEntity(EntityLayer.FOLIAGE, coord.x, coord.z) != null) {
+                                locations.add(coord);
+                            }
+                        }
+                    }
+
+                    if (!locations.isEmpty()) {
+                        var coord = locations.get(random.nextInt(locations.size()));
+                        new Grass(this.getWorld(), coord);
+                        this.removeFullness(1.0);
+                    }
+                }
+
+            }
         }
     }
 }
