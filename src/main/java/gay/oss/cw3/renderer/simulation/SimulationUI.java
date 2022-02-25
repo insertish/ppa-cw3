3206114dbf18f712ca81bd7647c7dc51ea9f755f
Paul Makles <paulmakles@gmail.com>
2022-02-25 19:56:28 +0000

    feat(ui): add font rendering

--- src/main/java/gay/oss/cw3/renderer/simulation/SimulationUI.java
+++ src/main/java/gay/oss/cw3/renderer/simulation/SimulationUI.java
@@ -3,7 +3,7 @@ package gay.oss.cw3.renderer.simulation;
 import org.joml.Vector4f;
 
 import gay.oss.cw3.renderer.objects.Texture;
-import gay.oss.cw3.renderer.shaders.UI;
+import gay.oss.cw3.renderer.ui.UI;
 import gay.oss.cw3.simulation.world.World;
 
 public class SimulationUI extends UI {
@@ -28,7 +28,13 @@ public class SimulationUI extends UI {
     }
 
     protected void drawUI() {
+        String text = this.world.getTime() + " ticks";
+
         this.drawRect(24, 24, 80, 80, new Vector4f(0, 0, 0, 0.5f));
+        this.drawRect(128, 24, 16 + 24 * text.length(), 40, new Vector4f(0, 0, 0, 0.5f));
+
         this.drawRect(32, 32, 64, 64, this.dayCycleTextures[this.world.getDayCycle().getIndex()]);
+
+        this.drawText(136, 32, 24, text);
     }
 }
---
+++ src/main/resources/shaders/ui/text.vert.glsl
@@ -0,0 +1,19 @@
+#version 330 core
+layout (location = 0) in vec3 vertexPos;
+layout (location = 1) in vec2 vertexUV;
+
+uniform mat4 model;
+uniform mat4 modelViewProjection;
+
+uniform int atlas;
+uniform int atlasX;
+uniform int atlasY;
+
+out vec2 fragUV;
+
+void main() {
+    gl_Position = modelViewProjection * vec4(vertexPos, 1.0);
+    fragUV = vertexUV / atlas;
+    fragUV.x += float(atlasX) / atlas;
+    fragUV.y += float(atlasY) / atlas;
+}
