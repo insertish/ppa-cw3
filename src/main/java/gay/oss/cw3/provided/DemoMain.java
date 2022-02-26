0038c9e7d1cc556d2290246aa11e120015e01e76
williambl <william@williambl.com>
2022-02-26 15:01:27 +0000

    chore: delete unused provided classes

--- src/main/java/gay/oss/cw3/provided/DemoMain.java
+++
@@ -1,8 +0,0 @@
-package gay.oss.cw3.provided;
-
-public class DemoMain {
-    // This runs the original simulation given.
-    public static void main(String[] args) {
-        new Simulator().runLongSimulation();
-    }
-}
