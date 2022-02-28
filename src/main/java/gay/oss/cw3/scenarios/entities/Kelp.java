216cca0706efc1812686aab74be2550e5897e4c1
williambl <william@williambl.com>
2022-02-28 18:38:16 +0000

    feat: improve y-offset of kelp & fish

--- src/main/java/gay/oss/cw3/scenarios/entities/Kelp.java
+++ src/main/java/gay/oss/cw3/scenarios/entities/Kelp.java
@@ -58,4 +58,11 @@ public class Kelp extends Entity {
             }
         }
     }
+
+    @Override
+    public float yOffset() {
+        var height = this.getWorld().getMap().getHeight(this.getLocation().x, this.getLocation().z);
+        var offset = this.getWorld().getMap().getWaterLevel() - height - 10f;
+        return height > height+offset ? 0f : offset;
+    }
 }
