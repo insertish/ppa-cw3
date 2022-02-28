40f8c707bfa9ca6264aafbc00d3a7b3f1bb6db3f
williambl <william@williambl.com>
2022-02-28 12:07:05 +0000

    docs: write javadoc for EntityAttributeMap

--- src/main/java/gay/oss/cw3/simulation/entity/EntityAttributeMap.java
+++ src/main/java/gay/oss/cw3/simulation/entity/EntityAttributeMap.java
@@ -4,6 +4,9 @@ import java.util.EnumMap;
 import java.util.Map;
 import java.util.Random;
 
+/**
+ * An Entity's EntityAttributeMap stores and provides its values for {@link EntityAttribute EntityAttributes}.
+ */
 public final class EntityAttributeMap {
     private final Map<EntityAttribute, Double> map = new EnumMap<>(EntityAttribute.class);
     private final Random random = new Random();
@@ -12,15 +15,35 @@ public final class EntityAttributeMap {
         this.map.putAll(map);
     }
 
+    /**
+     * Creates a new EntityAttributeMap which provides default values for all attributes.
+     */
     public EntityAttributeMap() {
     }
 
+    /**
+     * Sets the value of an attribute to the given value.
+     *
+     * <p>Will throw {@link IllegalArgumentException} if {@code value} is outside of the attribute's bounds.</p>
+     *
+     * @param attribute the attribute to set
+     * @param value     the value to set it to
+     */
     public void set(final EntityAttribute attribute, final double value) {
         checkBounds(attribute, value);
 
         this.map.put(attribute, value);
     }
 
+    /**
+     * Sets the value of an attribute to the given value with some random variation.
+     *
+     * <p>Will throw {@link IllegalArgumentException} if {@code value} is outside of the attribute's bounds.</p>
+     *
+     * @param attribute the attribute to set
+     * @param value     the value to set it to
+     * @param variation the factor of random variation to apply
+     */
     public void set(final EntityAttribute attribute, final double value, final double variation) {
         checkBounds(attribute, value);
 
@@ -29,10 +52,33 @@ public final class EntityAttributeMap {
         this.map.put(attribute, modifiedValue);
     }
 
+    /**
+     * Gets the value associated with the attribute, or else the default value of the attribute.
+     *
+     * @param attribute the attribute
+     *
+     * @return the value of the attribute
+     */
     public double get(final EntityAttribute attribute) {
         return this.map.getOrDefault(attribute, attribute.defaultValue);
     }
 
+    /**
+     * Populate this EntityAttributeMap with values based on that of two others. This is used to make the attributes
+     * of children be based on that of its parents.
+     *
+     *
+     * <p>For each attribute A:</p>
+     * <ul>
+     *     <li>If both parents have a value for A, the result is the average of their values plus random variation.</li>
+     *     <li>If one parent has a value for A, the result is that value plus some random variation.</li>
+     *     <li>If neither parent has a value for A, no value will be set for A.</li>
+     * </ul>
+     *
+     * @param parentA       one of the 'parent' attribute maps
+     * @param parentB       the other 'parent' attribute map
+     * @param randomness    the random variation factor
+     */
     public void inheritFromParents(final EntityAttributeMap parentA, final EntityAttributeMap parentB, final double randomness) {
         final Map<EntityAttribute, Double> results = new EnumMap<>(EntityAttribute.class);
 
