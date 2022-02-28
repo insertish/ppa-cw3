6915db34ffa637553e01eeeb63595bfc680c9392
williambl <william@williambl.com>
2022-02-28 12:22:51 +0000

    docs: write javadoc for Sex

--- src/main/java/gay/oss/cw3/simulation/entity/Sex.java
+++ src/main/java/gay/oss/cw3/simulation/entity/Sex.java
@@ -4,12 +4,29 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Predicate;
 
+/**
+ * Represents the sex of an entity.
+ */
 // binary moment :I
 public enum Sex {
+    /**
+     * The male sex.
+     */
     MALE,
+    /**
+     * The female sex.
+     */
     FEMALE,
     ;
 
+    /**
+     * Whether two sexes are compatible for producing children.
+     *
+     * @param a the first sex
+     * @param b the other sex
+     *
+     * @return  whether the two can produce children
+     */
     public static boolean isCompatible(@Nullable Sex a, @Nullable Sex b) {
         return a != null && b != null && a != b;
     }
